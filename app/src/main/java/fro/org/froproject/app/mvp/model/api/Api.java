79a16033e5d082215cb57b1abef7cdeaba9a784b
李光明 <510941969@qq.com>
2017-05-31 15:20:54 +0800

    first commit Initialization Fro project Created By Lgm

---
+++ app/src/main/java/fro/org/froproject/app/mvp/model/api/Api.java
@@ -0,0 +1,10 @@
+package fro.org.froproject.app.mvp.model.api;
+
+/**
+ * Created by Lgm on 2017/5/31 0031.
+ */
+
+public interface Api {
+    String APP_URL = "https://api.github.com";
+    String RequestSuccess = "0";
+}
---
+++ app/src/main/java/fro/org/froproject/app/mvp/model/api/service/CommonService.java
@@ -0,0 +1,10 @@
+package fro.org.froproject.app.mvp.model.api.service;
+
+/**
+ *
+ * 存放通用的api
+ * Created by Lgm on 2017/5/31 0031.
+ */
+
+public interface CommonService {
+}
