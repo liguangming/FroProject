78e46cab4fa8687371b2ee500a3493ed13c06ccf
李光明 <510941969@qq.com>
2017-05-31 15:55:25 +0800

    Modify package structure

--- app/src/main/java/fro/org/froproject/app/app/EventBusTag.java
+++
@@ -1,9 +0,0 @@
-package fro.org.froproject.app.app;
-
-/**
- * EventBus tag 常量
- * Created by Lgm on 2017/5/31 0031.
- */
-
-public interface EventBusTag {
-}
--- app/src/main/java/fro/org/froproject/app/mvp/model/api/cache/CommonCache.java
+++
@@ -1,8 +0,0 @@
-package fro.org.froproject.app.mvp.model.api.cache;
-
-/**
- * Created by Lgm on 2017/5/31 0031.
- */
-
-public interface CommonCache {
-}
